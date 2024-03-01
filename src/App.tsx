a9033b01923132a971bb164782e4137c2480fee3
maceq687 <maciek@softman.pl>
2024-03-01 17:24:14 +0100

    Fixed problem with while loop

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,24 @@ import "./App.css";
 function App() {
   const coins: number[] = [1, 2, 5];
   // 1 5 7 : 10
+  // 2 5 7 : 11
   const [state, setState] = useState(11);
   const coinsResult: string[] = [];
   let counting = true;
 
   const updateCoinValue = (event: React.ChangeEvent<HTMLInputElement>) => {
     setState(parseInt(event.target.value));
-    console.log(state);
     coins.sort(function (a, b) {
       return a - b;
     });
+    let remainingValue = state;
     const coinsOrdered: number[] = coins.reverse();
-    const biggestDivider: number = findBiggestDivider(
-      state,
-      coinsOrdered
-    ) as number;
     while (counting) {
-      let remainingValue = state;
+      const biggestDivider: number = findBiggestDivider(
+        remainingValue,
+        coinsOrdered
+      ) as number;
       remainingValue = divideByBD(remainingValue, biggestDivider) as number;
-      console.log(remainingValue);
     }
     console.log(coinsResult);
   };
@@ -36,10 +35,9 @@ function App() {
   function divideByBD(value: number, divider: number) {
     let result = value / divider;
     result = Math.floor(result);
-    console.log(result);
     coinsResult.push(divider + "*" + result);
     result = value - divider * result;
-    if (result != 0) return result;
+    if (result > 0) return result;
     else {
       counting = false;
     }
