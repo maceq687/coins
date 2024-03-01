84d610f62310472926fb3f2d0b8a3f72fd427655
maceq687 <maciek@softman.pl>
2024-03-01 13:24:58 +0100

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Coins</h1>
+      <div className="card">
+        <input type="text"></input>
+      </div>
+    </>
+  );
+}
+
+export default App;
